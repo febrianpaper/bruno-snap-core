b89ff6aa3cbc4d4b32cd1a3eec7ab0aa7f27a09e
febrianpaper <febriansyah.rayiid@paper.id>
2024-09-04 13:24:24 +0700

    add other uat bank transfer

--- run-uat-mandiri-pusat.sh
+++ run-uat-mandiri-pusat.sh
@@ -1,10 +1,12 @@
 echo "Running UAT Tools [Mandiri Pusat]"
 
 ENV="local"
-RESULT="mandiri-pusat-uat.json"
+RESULT="mandiri-pusat"
 
-ACTION=$(gum choose --header="Choose Action" "balance-inquiry" "internal-account-inquiry")
+ACTION=$(gum choose --header="Choose Action" "balance-inquiry" "internal-account-inquiry" "external-account-inquiry" "transfer-intrabank" "transfer-interbank" "transfer-rtgs")
 MODULE="Mandiri/${ACTION}/"
+
+RESULT="$RESULT-$ACTION-uat.json"
 
 touch result/$RESULT
 
